ca6ceacd67543efe126e43db40126f56e585c265
yokobond <koji.yokokawa@gmail.com>
2022-02-01 10:41:29 +0900

    Update version

--- src/gui/lib/libraries/extensions/entry/index.jsx
+++ src/gui/lib/libraries/extensions/entry/index.jsx
@@ -32,7 +32,7 @@ const entry = {
             defaultMessage: 'Connect Grove sensors and actuators.',
             description: 'Description for this extension',
             id: 'g2s.entry.description'
-        })} (v0.7.2)`;
+        })} (v0.8.0)`;
     },
     featured: true,
     disabled: false,
