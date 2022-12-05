b18b258fbf9ed6df50a3c23ae74265a5c2d1412d
yokobond <yokobond@users.noreply.github.com>
2022-12-05 03:43:23 +0000

    deploy: ae39c5f2bc68e2785474ab926e15402f2e1a70bc

---
+++ dev/src/gui/lib/libraries/extensions/entry/index.jsx
@@ -0,0 +1,49 @@
+/**
+ * This is an extension for Xcratch.
+ */
+
+import iconURL from './entry-icon.png';
+import insetIconURL from './inset-icon.png';
+import translations from './translations.json';
+
+/**
+ * Formatter to translate the messages in this extension.
+ * This will be replaced which is used in the React component.
+ * @param {object} messageData - data for format-message
+ * @returns {string} - translated message for the current locale
+ */
+let formatMessage = messageData => messageData.defaultMessage;
+
+const entry = {
+    get name () {
+        return formatMessage({
+            id: 'g2s.entry.name',
+            default: 'AkaDako',
+            description: 'name of the extension'
+        });
+    },
+    extensionId: 'g2s',
+    extensionURL: 'https://tfabworks.github.io/xcx-g2s/dist/g2s.mjs',
+    collaborator: 'TFabWorks',
+    iconURL: iconURL,
+    insetIconURL: insetIconURL,
+    get description () {
+        return `${formatMessage({
+            defaultMessage: 'Connect Grove sensors and actuators.',
+            description: 'Description for this extension',
+            id: 'g2s.entry.description'
+        })} (v0.10.2)`;
+    },
+    featured: true,
+    disabled: false,
+    bluetoothRequired: false,
+    internetConnectionRequired: false,
+    helpLink: 'https://tfabworks.com/akadako/',
+    setFormatMessage: formatter => {
+        formatMessage = formatter;
+    },
+    translationMap: translations
+};
+
+export {entry}; // loadable-extension needs this line.
+export default entry;
---
+++ dev/src/vm/extensions/block/akadako-connector.js
@@ -0,0 +1,171 @@
+import {EventEmitter} from 'events';
+import AkaDakoBoard from './akadako-board';
+
+/**
+ * Manager object which serves AkaDako boards.
+ */
+export class AkaDakoConnector extends EventEmitter {
+
+    /**
+     * Event name for reporting that a board removed.
+     * @const {string}
+     */
+    static get BOARD_REMOVED () {
+        return 'BOARD_REMOVED';
+    }
+
+    /**
+     * Event name for reporting that a board added.
+     * @const {string}
+     */
+    static get BOARD_ADDED () {
+        return 'BOARD_ADDED';
+    }
+
+    /**
+     * Constructor of this instance.
+     * @param {Runtime} runtime - Scratch runtime object
+     */
+    constructor (runtime) {
+        super();
+        
+        /**
+         * The Scratch 3.0 runtime.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+
+        /**
+         * Available boards
+         * @type {Array<AkaDakoBoard>}
+         */
+        this.boards = [];
+
+        /**
+         * Selection rules for MIDIPort
+         */
+        this.midiPortFilters = [
+            {manufacturer: null, name: /STEAM BOX/},
+            {manufacturer: null, name: /MidiDako/},
+            {manufacturer: null, name: /AkaDako/}
+        ];
+
+        /**
+         * Settings for WebSerial
+         */
+        this.serialPortOptions = {
+            filters: [
+                {usbVendorId: 0x04D8, usbProductId: 0xE83A}, // Licensed for AkaDako
+                {usbVendorId: 0x04D8, usbProductId: 0x000A}, // Dev board
+                {usbVendorId: 0x04D9, usbProductId: 0xB534} // Use in the future
+            ]
+        };
+    }
+
+    /**
+     * Return connected board which is confirmed with the options.
+     * @param {object} options serial port options
+     * @param {Array<{usbVendorId, usbProductId}>} options.filters allay of filters
+     * @returns {AkaDakoBoard?} first board which confirmed with options
+     */
+    findBoard (options) {
+        if (this.boards.length === 0) return;
+        if (!options || !options.filters) return this.boards[0];
+        return this.boards.find(aBoard => (aBoard.isConnected() && options.filters.some(filter =>
+            ((filter.usbVendorId === aBoard.portInfo.usbVendorId) &&
+                    (filter.usbProductId === aBoard.portInfo.usbProductId)))));
+    }
+
+    /**
+     * Add a board to the boards holder.
+     * @param {AkaDakoBoard} newBoard the board to be added
+     */
+    addBoard (newBoard) {
+        this.boards.push(newBoard);
+        this.emit(AkaDakoConnector.BOARD_ADDED, newBoard);
+    }
+
+    /**
+     * Remove a board from the boards holder.
+     * @param {AkaDakoBoard} removal the board to be removed
+     */
+    removeBoard (removal) {
+        const indexOfRemoval = this.boards.indexOf(removal);
+        if (indexOfRemoval < 0) return; // not found
+        this.boards.splice(indexOfRemoval, 1);
+        this.emit(AkaDakoConnector.BOARD_ADDED, removal);
+    }
+
+    /**
+     * Return a connected AkaDako board
+     * @param {string} extensionId - ID of the extension which is requesting
+     * @returns {Promise<AkaDakoBoard>} a Promise which resolves a connected AkaDako board or reject with reason
+     */
+    connectedBoard (extensionId) {
+        const connectedBoard = this.findBoard();
+        if (connectedBoard) {
+            // share a board object
+            return Promise.resolve(connectedBoard);
+        }
+        return this.connectMIDI(extensionId)
+            .catch(reason => {
+                console.log(reason);
+                return this.connectSerial(extensionId);
+            });
+    }
+
+    connectSerial (extensionId) {
+        if (!('serial' in navigator)) {
+            return Promise.reject('This browser does not support Web Serial API.');
+        }
+        const newBoard = new AkaDakoBoard(this.runtime);
+        newBoard.extensionId = extensionId;
+        return newBoard.connectSerial(this.serialPortOptions)
+            .then(connected => {
+                this.addBoard(connected);
+                connected.once(AkaDakoBoard.RELEASED, () => {
+                    this.removeBoard(connected);
+                    this.runtime.emit(this.runtime.constructor.PERIPHERAL_DISCONNECTED, {
+                        name: connected.name,
+                        path: connected.portInfo
+                    });
+                });
+                return connected;
+            });
+    }
+
+    /**
+     * Return a connected AkaDako board via MIDI interface
+     * @param {string} extensionId - ID of the extension which is requesting
+     * @returns {Promise<AkaDakoBoard>} a Promise which resolves a connected AkaDako board or reject with reason
+     */
+    connectMIDI (extensionId) {
+        const newBoard = new AkaDakoBoard(this.runtime);
+        newBoard.extensionId = extensionId;
+        return newBoard.connectMIDI(this.midiPortFilters)
+            .then(connected => {
+                this.addBoard(connected);
+                connected.once(AkaDakoBoard.RELEASED, () => {
+                    this.removeBoard(connected);
+                    this.runtime.emit(this.runtime.constructor.PERIPHERAL_DISCONNECTED, {
+                        name: connected.name,
+                        path: connected.portInfo
+                    });
+                });
+                return connected;
+            });
+    }
+
+}
+
+/**
+ * Return a shared AkaDako connector object
+ * @param {Runtime} runtime - Scratch runtime object
+ * @returns {AkaDakoConnector} a AkaDako connector object
+ */
+export const getAkaDakoConnector = runtime => {
+    if (!runtime.akaDakoConnector) {
+        runtime.akaDakoConnector = new AkaDakoConnector(runtime);
+    }
+    return runtime.akaDakoConnector;
+};
