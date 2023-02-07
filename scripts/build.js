10da56e63e2e3481d34ad0ffe47c80637d1fde82
yokobond <koji.yokokawa@gmail.com>
2023-02-07 16:14:12 +0900

    Add build process for a serial disabled module.

---
+++ scripts/build.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+'use strict'
+
+/**
+ * This is a script to build g2s module files.
+ * Its process removes ./build to use as a temporary storage.
+ * ./dist is a destination directory for the modules.
+ */
+
+const path = require('path');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+
+const targetDir = process.cwd();
+const connectorFilePath = path.resolve(targetDir, 'src/vm/extensions/block/akadako-connector.js');
+const connectorBackupFilePath = path.resolve(targetDir, 'src/vm/extensions/block/_akadako-connector.js');
+
+// Build a normal module
+execSync(`npx xcratch-build --module=g2s --gui=../scratch-gui --block=./src/vm/extensions/block --entry=./src/gui/lib/libraries/extensions/entry --output=./build`);
+console.log(`Built a module`);
+fs.copyFileSync('./build/g2s.mjs', './dist/g2s.mjs')
+
+// Build a serial disabled module
+let originalCode = fs.readFileSync(connectorFilePath, 'utf-8');
+originalCode = originalCode.replace(/this.useSerial\s+=\s+[^;]+;/m, `this.useSerial = false;`);
+fs.renameSync(connectorFilePath, connectorBackupFilePath);
+fs.writeFileSync(connectorFilePath, originalCode);
+console.log(`Modify code for disable serial in ${connectorFilePath}`);
+try {
+    execSync(`npx xcratch-build --module=g2s-m --gui=../scratch-gui --block=./src/vm/extensions/block --entry=./src/gui/lib/libraries/extensions/entry --output=./build`);
+    console.log(`Built a serial disabled module`);
+} catch (err) {
+    console.error(err);
+}
+fs.renameSync(connectorBackupFilePath, connectorFilePath);
+console.log(`Restore the code to ${connectorFilePath}`);
+fs.copyFileSync('./build/g2s-m.mjs', './dist/g2s-m.mjs')
