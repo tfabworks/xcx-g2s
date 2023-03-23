8d806e9ab292351fca218e89b11bd0cc9e9e1487
yokobond <yokobond@users.noreply.github.com>
2023-03-23 02:53:32 +0000

    deploy: c7c5c83c47649fa80e8a9fb004705c5e5c2db1da

--- src/vm/extensions/block/akadako-board.js
+++ src/vm/extensions/block/akadako-board.js
@@ -12,6 +12,7 @@ import {
     PIXEL_SET_PIXEL,
     PIXEL_SHOW
 } from './node-pixel-constants';
+import Servo from './servo';
 
 const Firmata = bindTransport.Firmata;
 
@@ -197,6 +198,12 @@ class AkaDakoBoard extends EventEmitter {
          * @type {number}
          */
         this.defaultNeoPixelLength = 3;
+
+        /**
+         * Servo motors on this board.
+         * @type {Array<Servo>}
+         */
+        this.servo = [];
     }
 
     /**
@@ -603,6 +610,13 @@ class AkaDakoBoard extends EventEmitter {
      * @returns {Promise} a Promise which resolves when the message was sent
      */
     digitalWrite (pin, value, enqueue) {
+        if (this.firmata.pins[pin].value === value) {
+            // to avoid chattering of the relay
+            return new Promise(resolve => {
+                setTimeout(() => resolve(), this.sendingInterval);
+            });
+        }
+        this.firmata.pinMode(pin, this.firmata.MODES.OUTPUT);
         return new Promise(resolve => {
             this.firmata.digitalWrite(pin, value, enqueue);
             setTimeout(() => resolve(), this.sendingInterval);
@@ -620,6 +634,27 @@ class AkaDakoBoard extends EventEmitter {
             this.firmata.pwmWrite(pin, value);
             setTimeout(() => resolve(), this.sendingInterval);
         });
+    }
+
+    /**
+     * Return servo object on the pin
+     * @param {number} pin - pin number of the servo
+     * @returns {Servo?} servo which is connected on the pin
+     */
+    getServo (pin) {
+        if (this.version.type === 2) {
+            // STEAM Tool
+            if (pin === 6 || pin === 9) {
+                // These pins are used for on-board buttons in the STEAM tool.
+                return null;
+            }
+        }
+        let servo = this.servo[pin];
+        if (!servo) {
+            servo = new Servo(this, pin);
+            this.servo[pin] = servo;
+        }
+        return servo;
     }
 
     /**
