4f01eb65ed1d101b2355dc7a148f6c22f24f38f4
yokobond <yokobond@users.noreply.github.com>
2023-04-25 08:08:37 +0000

    deploy: 7bf2900ad5aa48d909d9ab2ab537034234c069ac

--- src/gui/lib/libraries/extensions/entry/index.jsx
+++ src/gui/lib/libraries/extensions/entry/index.jsx
@@ -6,7 +6,7 @@ import iconURL from './entry-icon.png';
 import insetIconURL from './inset-icon.png';
 import translations from './translations.json';
 
-const version = 'v1.1.1';
+const version = 'v1.1.2';
 
 /**
  * Formatter to translate the messages in this extension.
