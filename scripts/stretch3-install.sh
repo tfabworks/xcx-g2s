fd0dd662cbbe614aedd9b83440fe97afdbce616d
Yoshiaki Kawazu <kawazzz@gmail.com>
2024-09-27 15:44:39 +0900

    stretch3のインストールスクリプトを冪等にして、stretch3の起動スクリプトを追加した

--- scripts/stretch3-install.sh
+++ scripts/stretch3-install.sh
@@ -8,26 +8,27 @@
 
 LF=$(printf '\\\012_')
 LF=${LF%_}
-EXTENSION_NAME=AkaDako
-EXTENSION_REP=xcx-g2s
+
+EXTENSION_REPO=xcx-g2s
 EXTENSION_ID=g2s
-COLLABORATOR="TFabWorks"
-EXTENSION_DESCRIPTION="Connect Grove sensors and actuators."
 
-### register it as a builtin extenstion
+# register to vm (as a builtin extenstion)
 mkdir -p node_modules/scratch-vm/src/extensions/${EXTENSION_ID}
-cp ${EXTENSION_REP}/dist/${EXTENSION_ID}.mjs node_modules/scratch-vm/src/extensions/${EXTENSION_ID}/
-mv node_modules/scratch-vm/src/extension-support/extension-manager.js node_modules/scratch-vm/src/extension-support/extension-manager.js_orig
-sed -e "s|class ExtensionManager {|builtinExtensions['${EXTENSION_ID}'] = () => {${LF}    const formatMessage = require('format-message');${LF}    const ext = require('../extensions/${EXTENSION_ID}/${EXTENSION_ID}.mjs');${LF}    const blockClass = ext.blockClass;${LF}    blockClass.formatMessage = formatMessage;${LF}    return blockClass;${LF}};${LF}${LF}class ExtensionManager {|g" node_modules/scratch-vm/src/extension-support/extension-manager.js_orig > node_modules/scratch-vm/src/extension-support/extension-manager.js
+cp ${EXTENSION_REPO}/dist/${EXTENSION_ID}.mjs node_modules/scratch-vm/src/extensions/${EXTENSION_ID}/
+if ! grep -q -F "builtinExtensions['${EXTENSION_ID}']" node_modules/scratch-vm/src/extension-support/extension-manager.js; then
+    cp node_modules/scratch-vm/src/extension-support/extension-manager.js node_modules/scratch-vm/src/extension-support/extension-manager.js_orig
+    sed -e "s|class ExtensionManager {|builtinExtensions['${EXTENSION_ID}'] = () => {${LF}    const formatMessage = require('format-message');${LF}    const ext = require('../extensions/${EXTENSION_ID}/${EXTENSION_ID}.mjs');${LF}    const blockClass = ext.blockClass;${LF}    blockClass.formatMessage = formatMessage;${LF}    return blockClass;${LF}};${LF}${LF}class ExtensionManager {|g" node_modules/scratch-vm/src/extension-support/extension-manager.js_orig > node_modules/scratch-vm/src/extension-support/extension-manager.js
+fi
 
-
-### copy entry files
+# register to gui (extension jsx)
 mkdir -p src/lib/libraries/extensions/${EXTENSION_ID}
-cp ${EXTENSION_REP}/src/gui/lib/libraries/extensions/entry/index-stretch.jsx src/lib/libraries/extensions/${EXTENSION_ID}/index.jsx
-cp ${EXTENSION_REP}/src/gui/lib/libraries/extensions/entry/translations.json src/lib/libraries/extensions/${EXTENSION_ID}/
-cp ${EXTENSION_REP}/src/gui/lib/libraries/extensions/entry/entry-icon.png src/lib/libraries/extensions/${EXTENSION_ID}/
-cp ${EXTENSION_REP}/src/gui/lib/libraries/extensions/entry/inset-icon.png src/lib/libraries/extensions/${EXTENSION_ID}/
-
-### insert it to the library
-mv src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
-sed -e "s|^export default \[$|import ${EXTENSION_ID}Entry from './${EXTENSION_ID}/index.jsx';${LF}${LF}export default [${LF}    ${EXTENSION_ID}Entry,|g" src/lib/libraries/extensions/index.jsx_orig > src/lib/libraries/extensions/index.jsx
+cp ${EXTENSION_REPO}/src/gui/lib/libraries/extensions/entry/entry-icon.* src/lib/libraries/extensions/${EXTENSION_ID}/
+cp ${EXTENSION_REPO}/src/gui/lib/libraries/extensions/entry/inset-icon.* src/lib/libraries/extensions/${EXTENSION_ID}/
+cp ${EXTENSION_REPO}/src/gui/lib/libraries/extensions/entry/translations.json src/lib/libraries/extensions/${EXTENSION_ID}/
+#cp ${EXTENSION_REPO}/src/gui/lib/libraries/extensions/entry/index-base.jsx src/lib/libraries/extensions/${EXTENSION_ID}/
+#cp ${EXTENSION_REPO}/src/gui/lib/libraries/extensions/entry/index-stretch3.jsx src/lib/libraries/extensions/${EXTENSION_ID}/index.jsx
+cp ${EXTENSION_REPO}/src/gui/lib/libraries/extensions/entry/index-stretch.jsx src/lib/libraries/extensions/${EXTENSION_ID}/index.jsx
+if ! grep -q "import ${EXTENSION_ID}Entry from" src/lib/libraries/extensions/index.jsx; then
+    cp src/lib/libraries/extensions/index.jsx src/lib/libraries/extensions/index.jsx_orig
+    sed -e "s|^export default \[$|import ${EXTENSION_ID}Entry from './${EXTENSION_ID}/index.jsx';${LF}${LF}export default [${LF}    ${EXTENSION_ID}Entry,|g" src/lib/libraries/extensions/index.jsx_orig > src/lib/libraries/extensions/index.jsx
+fi
